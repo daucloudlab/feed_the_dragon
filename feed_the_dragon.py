077c2d969e34c05caa5a3b9c38c1259999244d48
Daulet <daucloudlab@gmail.com>
2021-11-20 19:06:25 +0600

    setup the game: load texts, images, sounds

--- feed_the_dragon.py
+++ feed_the_dragon.py
@@ -1,26 +1,103 @@
-import pygame
+import pygame, random
 
 WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 600
+WINDOW_HEIGHT = 400
 
 pygame.init()
 
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Feed the Dragon")
 
+# variables and constants
+BUFFER_DISTANCE = 100
+PLAYER_STARTING_LIVES = 5
+PLAYER_VELOCITY = 5
+COIN_STARTING_VELOCITY = 5
+COIN_ACCELERATION = 0.5
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
 
 
+# colors
+GREEN = (0, 255, 0)
+DARKGREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# sounds and musics
+miss_sound = pygame.mixer.Sound('miss_sound.wav')
+miss_sound.set_volume(0.1)
+
+coin_sound = pygame.mixer.Sound('coin_sound.wav')
+
+pygame.mixer.music.load('ftd_background_music.wav')
+
+# images
+player_image = pygame.image.load('dragon_right.png')
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT//2
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.x = WINDOW_WIDTH+BUFFER_DISTANCE
+coin_rect.y = random.randint(64, WINDOW_HEIGHT-32)
+
+#fonts
+font = pygame.font.Font('AttackGraffiti.ttf', 32)
+
+score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+title_text = font.render("Feed the Dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH//2
+title_rect.y = 10
+
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH-10, 10)
+
+game_over_text = font.render("GAMEOVER", True, GREEN, DARKGREEN)
+game_over_rect = game_over_text.get_rect()
+game_over_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT)
+
+continue_text = font.render("Press any key to play again",
+    True, GREEN, DARKGREEN)
+continue_rect = continue_text.get_rect()
+continue_rect.center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2-100)
+
+# clock
 FPS = 60
 clock = pygame.time.Clock()
 
+# game cycle
+pygame.mixer.music.play(-1, 0.0)
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    display_surface.fill((0,0,0))
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_rect.top > 64:
+        player_rect.y -= PLAYER_VELOCITY
+    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
+        player_rect.y += PLAYER_VELOCITY
 
+    display_surface.fill(BLACK)
+    
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    pygame.draw.line(display_surface, WHITE, (0,64), (WINDOW_WIDTH, 64), 2)
+    
+    display_surface.blit(player_image, player_rect)
+    display_surface.blit(coin_image, coin_rect)
+    
     pygame.display.update()
     clock.tick(FPS)
 
